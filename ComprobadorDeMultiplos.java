2e3de2a19e16ad6149d90cd9d7752fa2960bcdd7
Patricia <p.santamartamedina@gmail.com>
2020-12-11 14:17:00 +0100

    02 pasar test

--- ComprobadorDeMultiplos.java
+++ ComprobadorDeMultiplos.java
@@ -4,23 +4,23 @@ public class ComprobadorDeMultiplos{
         
     public boolean esMultiploDe2(int numero){
         boolean respuesta = false;
-        if (numero % 2 == 0){
+        if ((numero % 2 == 0) && (numero >= 2)){
             respuesta = true;
-        }
+           }        
         return respuesta;
     } 
 
     public boolean esMultiploDe5(int numero){
         boolean respuesta = false;
-        if (numero % 5 == 0){
+        if ((numero % 5 == 0) && (numero >= 5)){
             respuesta = true;
         }
         return respuesta;
     }
     
-    public boolean esMultiploDe(int numero, int divisor){   
+    public boolean esMultiploDe(int a, int b){   
         boolean respuesta = false;
-        if (numero % divisor == 0){
+        if ((a >= b) && (b > 0) && (a >= 0) && (b != 0) && (a % b == 0)){
             respuesta = true;
         }
         return respuesta;
